e53a9c90d31f36e708a64245b41880027b70705f
mteresacastro <m.teresacastrogonzalez@gmail.com>
2024-05-26 15:42:57 +0200

    add spinner

---
+++ src/app/shared/components/lazyImage/lazyImage.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'shared-lazy-image',
+  templateUrl: './lazyImage.component.html'
+})
+
+export class LazyImageComponent implements OnInit {
+
+  @Input()
+  public url!: string;
+
+  @Input()
+  public alt: string = "";
+
+  public hasLoaded: boolean = false;
+
+  ngOnInit(): void {
+      if(!this.url) throw new Error('URL property is required');
+  }
+
+  onLoad(): void {
+    console.log('image loaded');
+    this.hasLoaded = true;
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { LazyImageComponent } from './components/lazyImage/lazyImage.component';
 
 
 
 @NgModule({
   declarations: [
-    SidebarComponent
+    SidebarComponent,
+    LazyImageComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    SidebarComponent
+    SidebarComponent,
+    LazyImageComponent
   ]
 })
 export class SharedModule { }
